860f751ba6feb16ed5427dc46e3fb2e8f8dd1f84
Guilherme Bessa <128107879+Guiezz@users.noreply.github.com>
2024-09-17 10:26:51 -0300

    feat: logo back to home page

--- src/components/base/header.tsx
+++ src/components/base/header.tsx
@@ -7,14 +7,14 @@ import { Button } from '../ui/button'
 export function Header() {
   return (
     <header className="flex h-20 w-full items-center justify-between px-10">
-      <div className="flex items-center gap-10">
+      <Link href={'/'} className="flex items-center gap-10">
         <div className="flex gap-2">
           <Package size={28} className="text-slate-600" />
           <h1 className="font-semibold text-2xl text-slate-600">Deck</h1>
         </div>
 
         <SearchInputWithFilters />
-      </div>
+      </Link>
 
       <Button asChild>
         <Link href="/login">Entrar</Link>
